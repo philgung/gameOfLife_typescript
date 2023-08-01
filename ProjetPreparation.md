5e0f081ab2fd5016efe42deb3d8b180b495bae73
philippe gung <phil.gung@gmail.com>
2023-08-01 16:49:54 +0200

    Initialisation repo + first acceptance test

---
+++ ProjetPreparation.md
@@ -0,0 +1,7 @@
+﻿
+````
+mkdir tdd-ts & cd tdd-ts
+npm init
+npm install -D typescript @types/jest ts-node ts-jest
+npx tsc -init
+````
---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+﻿import { Config } from '@jest/types';
+
+const config: Config.InitialOptions ={
+    verbose:true,
+    testEnvironment: "node",
+    preset: 'ts-jest',
+};
+export default config;
