6527f4dad3a57cbf39d5b2754496b5bf5b2ebd80
philippe gung <phil.gung@gmail.com>
2023-08-01 17:31:18 +0200

    clean

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -1,4 +1,18 @@
-﻿export function generate(startingPosition: string): string {
+﻿
+// Cell 
+//      (live/dead)
+//      Neighbours (cells)
+
+// Grid of cells
+//      Import Starting Position (ctor ??)
+//      Generate -> next Position
+//      Current : Position
+
+// Position (immutable)
+//      Display output
+//      override equals ?
+
+export function generate(startingPosition: string): string {
     return `........
             ...**...
             ...**...
--- tests/gameOfLife.spec.ts
+++ tests/gameOfLife.spec.ts
@@ -8,12 +8,22 @@ describe('GameOfLife - Acceptance tests', () => {
             ....*...
             ...**...
             ........`;
+        
+        const nextPosition = generate(startingPosition);
+        
         const outputPosition =
             `........
             ...**...
             ...**...
             ........`;
-        const nextPosition = generate(startingPosition);
         expect(nextPosition).toBe(outputPosition);
    }) 
 });
+
+
+// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+// Any live cell with more than three live neighbours dies, as if by overcrowding.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any dead cell with exactly three live neighbours becomes a live cell. 
+// You should write a program that can accept an arbitrary grid of cells, and will 
+// output a similar grid showing the next generation.
