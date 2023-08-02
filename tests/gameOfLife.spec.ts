f29197032eb56f0563f90db2ac46196c6c6861dd
philippe gung <phil.gung@gmail.com>
2023-08-02 17:03:09 +0200

    add new test : Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.

--- tests/gameOfLife.spec.ts
+++ tests/gameOfLife.spec.ts
@@ -1,16 +1,16 @@
 ﻿import {GameOfLife} from "../src/gameOfLife";
 import {Position} from "../src/Position";
 
-describe('GameOfLife - Acceptance tests', () => {   
+describe('GameOfLife - Acceptance tests', () => {
     it('Generation 1 -> Generation 2', () => {
         const initial =
             new Position(
-        `........
+                `........
             ....*...
             ...**...
             ........`);
-        
-        let game = new GameOfLife(initial);        
+
+        let game = new GameOfLife(initial);
         const next = game.Generate();
 
         const expectedOutput =
@@ -19,11 +19,28 @@ describe('GameOfLife - Acceptance tests', () => {
             ...**...
             ........`;
         expect(next.toString()).toBe(expectedOutput);
-   }); 
+    });
 });
 
+describe('Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.', () => {
+    it('A live cell with none live neighbours has to die', () => {
+        const initial =
+            new Position(
+                `...
+                    .*.
+                    ...`);
 
-// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+        let game = new GameOfLife(initial);
+        const next = game.Generate();
+
+        const expectedOutput =
+            `...
+            ...
+            ...`;
+        expect(next.toString()).toBe(expectedOutput);
+    });
+})
+
 // Any live cell with more than three live neighbours dies, as if by overcrowding.
 // Any live cell with two or three live neighbours lives on to the next generation.
 // Any dead cell with exactly three live neighbours becomes a live cell. 
